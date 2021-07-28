bc3ae20dab231d1861d93d75d0276b3e6d03138c
nwtsk <87970542+nwtsk@users.noreply.github.com>
2021-07-28 11:02:55 +0700

    Update solution.js

--- task_1/solution.js
+++ task_1/solution.js
@@ -7,6 +7,7 @@ function addInCartMessage(title, price) {
     // создайте переменную message
     let message
     // присвойте переменной message значение переменной productName
+    message = productName;
     // добавьте в конец переменной message пробел
     message = message + ' ';
     // добавьте в конец переменной message текст 'за'
@@ -14,6 +15,7 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message пробел
     message = message + ' ';
     // добавьте в конец переменной message значение переменной productPrice
+    message = message + price;
     // добавьте в конец переменной message пробел
     message = message + ' ';
     // добавьте в конец переменной message текст 'теперь в корзине!'
@@ -51,6 +53,7 @@ function addInCartSumChange(sum, delta) {
     let newSumText;
     newSumText = alert(newSum) + ' ₽'; 
     // замените склеивание, используемое в newSum Text на шаблонную строку
+    newSumText = `${newSum} ₽`
     // Конец решения задания №1.3.
 
     return newSumText;
