df502cda66cdbfc1554ec78678a5e2f7ea8c1b6a
nwtsk <87970542+nwtsk@users.noreply.github.com>
2021-07-28 11:02:46 +0700

    Update solution.js

--- task_2/solution.js
+++ task_2/solution.js
@@ -2,20 +2,29 @@ function calcShipping(sum, min, shipping) {
     let productsSum = sum; // сумма в корзине
     let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
     let shippingPrice = shipping; // стоимость доставки
-
+    let shippingSum;
     // Задание №2.1. Рассчитать доставку
-
     // создайте переменную shippingSum
-
+  
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
-
+    if (productsSum=0) 
+        { 
+            shippingSum = 0;
+        }
+ 
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
-
+    if (productsSum >= freeShippingMinSum)
+        {
+            shippingSum = 0 ;
+        }
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-
+    if (productsSum>0 && productsSum < freeShippingMinSum)
+        {
+        shippingSum = shippingPrice;
+        }
     // Конец решения задания №2.1.
 
     return shippingSum;
@@ -29,7 +38,7 @@ function calcDiscount(sum, min, discount) {
     // Задание №2.2. Рассчитать скидку
 
     // создайте переменную discountPrice
-
+    let discountPrice;
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
