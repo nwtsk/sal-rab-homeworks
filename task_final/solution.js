513c5468bdf6c3c38a72c6f296a47b3d12b1d8ca
nwtsk <87970542+nwtsk@users.noreply.github.com>
2021-08-01 23:08:43 +0700

    Update solution.js

--- task_final/solution.js
+++ task_final/solution.js
@@ -23,28 +23,25 @@
 //      "order": {
 //        "address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53",
 //        "sum": 900
-//       },
+//                },
 //       "goods": [
-//         {
+//                  {
 //           "title": "Пицца",
 //           "count": 2
-//         }
-//      ]
-//    }
+//                  }
+//                ]
+//              }
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
-
-    data.order.address = address;
-    data.client = name + phone;
-    data.order.sum = sum;
-
-    let jsonData = JSON.stringify(data);
-
+	
+	let data = { client: name + ' ' + phone, order: { address: "",  sum: sum}, goods: [] };
+	for(let i =0; i < goods.length; i++) {
+		let item = { title: goods[i].title, count: goods[i].count };
+		data.goods.push(item);
+	}
+    data.order.address = 'ул. ' + address.street + ', дом ' + address.house + ', ' + address.entrance + ' подъезд, ' + address.floor +  ' этаж, кв ' + address.flat;
+   
+    let jsonData = JSON.stringify({ data: data});
     return jsonData;
 }
